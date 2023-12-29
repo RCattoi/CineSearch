4d29fc815cc0a007a3a6ccd848b1c8608d2de0a4
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2023-12-29 10:43:21 -0300

    feat: add background

---
+++ src/components/background.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+import style from '../styles/background.module.css';
+
+function Background() {
+  return (
+    <>
+      <div className={style.background}>a</div>
+    </>
+  );
+}
+
+export default Background;
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Background from './components/background';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Background />
+  </React.StrictMode>
+);
