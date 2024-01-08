c3678ec9671e39d84199e318ff23968d176f703e
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-01-08 15:42:54 -0300

    fix: searchbar position

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import useFetch from '../hooks/useFecth';
+import Card from '../components/card';
 import Style from '../styles/search.module.css';
-function SearchBar() {
+
+const SearchBar = () => {
+  const [search, setSearch] = useState('');
+  const [searchMethod, setSearchMethod] = useState('load');
+
+  const { request } = useFetch();
+
+  useEffect(() => {
+    const callBack = async () => {
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZGU4NGYwYWVkYmVhZGQyMjRkNGU2ZGE1ZDVhNzAzNiIsInN1YiI6IjY1OGMyMTZiYjY4NmI5MjA0ZWRlNTlhZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6XIc8wLH8Xp6Lp_3gC9V8oElHWgusJFJciEpfjZCH2g',
+        },
+      };
+      const fetchData = await request(
+        'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
+        options
+      );
+      setSearch(fetchData.json.results.slice(0, 12));
+    };
+    callBack();
+  }, [request]);
   return (
     <>
-      <div className={Style.teste}>
-        <div className={Style.flex}>
-          <div>
+      <div className={Style.flex}>
+        <div>
           <input
             className={Style.search}
             type="text"
@@ -13,11 +38,11 @@ function SearchBar() {
             autoFocus
           ></input>
           <button className={Style.btn}>Search</button>
-          </div>
         </div>
       </div>
+      <Card data={search} type={searchMethod} />
     </>
   );
-}
+};
 
 export default SearchBar;
