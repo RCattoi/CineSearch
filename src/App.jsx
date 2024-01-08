29a162f3b01ca1784ed9c82dff2da4997312e931
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-01-08 15:39:27 -0300

    feat: create app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Background from './components/background';
+import SearchBar from './components/searchBar';
+
+import './styles/globalStyle.css';
+
+const App = () => {
+  return (
+    <>
+      <Background />
+      <SearchBar />
+    </>
+  );
+};
+
+export default App;
