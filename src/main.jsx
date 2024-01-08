3f21b1c4b46f5643ae8a46593aca85a75492bca0
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-01-08 15:40:20 -0300

    fix: change code from main.jsx to app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Background from './components/background';
-import SearchBar from './components/searchBar';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Background />
-    <SearchBar />
-  </React.StrictMode>
+  <>
+    <App />
+  </>
 );
