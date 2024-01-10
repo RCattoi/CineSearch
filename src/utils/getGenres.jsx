0c5e4e6fe31f2f3765fda02be53aaacfe5c6871b
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-01-10 19:09:05 -0300

    feat: create getGenres function to determine genres of movies

---
+++ src/utils/getGenres.jsx
@@ -0,0 +1,95 @@
+const genresName = (movieGen) => {
+  const genresData = {
+    genres: [
+      {
+        id: 28,
+        name: 'Action',
+      },
+      {
+        id: 12,
+        name: 'Adventure',
+      },
+      {
+        id: 16,
+        name: 'Animation',
+      },
+      {
+        id: 35,
+        name: 'Comedy',
+      },
+      {
+        id: 80,
+        name: 'Crime',
+      },
+      {
+        id: 99,
+        name: 'Documentary',
+      },
+      {
+        id: 18,
+        name: 'Drama',
+      },
+      {
+        id: 10751,
+        name: 'Family',
+      },
+      {
+        id: 14,
+        name: 'Fantasy',
+      },
+      {
+        id: 36,
+        name: 'History',
+      },
+      {
+        id: 27,
+        name: 'Horror',
+      },
+      {
+        id: 10402,
+        name: 'Music',
+      },
+      {
+        id: 9648,
+        name: 'Mystery',
+      },
+      {
+        id: 10749,
+        name: 'Romance',
+      },
+      {
+        id: 878,
+        name: 'Science Fiction',
+      },
+      {
+        id: 10770,
+        name: 'TV Movie',
+      },
+      {
+        id: 53,
+        name: 'Thriller',
+      },
+      {
+        id: 10752,
+        name: 'War',
+      },
+      {
+        id: 37,
+        name: 'Western',
+      },
+    ],
+  };
+
+  const resp = [];
+  movieGen.forEach((e) => {
+    for (let i = 0; i < genresData.genres.length; i++) {
+      if (genresData.genres[i].id === e) {
+        resp.push(genresData.genres[i]);
+      }
+    }
+  });
+
+  return resp;
+};
+
+export default genresName;
